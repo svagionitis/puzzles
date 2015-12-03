371dce56e7ce0788bc3f58df2c963fd0ef81646e
Stavros Vagionitis <svagionitis@tycoint.com>
2015-12-03 18:06:02 +0000

    Day 3: Fix the solution using a grid

--- adventofcode.com/3/sol3.py
+++ adventofcode.com/3/sol3.py
@@ -5,61 +5,30 @@ if __name__ == "__main__":
         d = f.read()
         data = d.strip('\n')
 
-        S = 0
-        N = 0
-        E = 0
-        W = 0
-        prev_d = ''
+        grid = [[0]*len(data) for x in range(len(data))]
+        x = 0
+        y = 0
+        grid[0][0] += 1
         for i, d in enumerate(data):
 
-            print '%d %s -- %d' % (i, d, len(data))
-
             if d == '^':
-                N += 1
-
-                if i ==  0:
-                    S += 1
-
-                if S and i and prev_d == 'v':
-                    S -= 1
-
-                print "S: %d N: %d E: %d W: %d -- %d" % (S, N, E, W, S+N+E+W)
+                y += 1
 
             if d == 'v':
-                S += 1
-
-                if i == 0:
-                    N += 1
-
-                if N and i and prev_d == '^':
-                    N -= 1
-
-                print "S: %d N: %d E: %d W: %d -- %d" % (S, N, E, W, S+N+E+W)
+                y -= 1
 
             if d == '>':
-                E += 1
+                x += 1
 
-                if i == 0:
-                    W += 1
-
-                if W and i and prev_d == '<':
-                    W -= 1
-
-                print "S: %d N: %d E: %d W: %d -- %d" % (S, N, E, W, S+N+E+W)
 
             if d == '<':
-                W += 1
+                x -= 1
 
-                if i == 0:
-                    E += 1
+            grid[x][y] += 1
 
-                if E and i and prev_d == '>':
-                    E -= 1
 
-                print "S: %d N: %d E: %d W: %d -- %d" % (S, N, E, W, S+N+E+W)
+        res = sum(sum(1 for i in row if i) for row in grid)
 
-            prev_d = d
-
-        print "Total S: %d N: %d E: %d W: %d -- %d" % (S, N, E, W, S+N+E+W)
+        print "1. How many houses receive at least one present? %d" % res
 
 
