8d26a9bb2ba012d1a20bf99e0dee98ecdc008683
Stavros Vagionitis <svagionitis@tycoint.com>
2015-12-08 18:28:54 +0000

    Day 8: Some helpful code

--- adventofcode.com/8/sol8.py
+++ adventofcode.com/8/sol8.py
@@ -1,4 +1,5 @@
 import re
+import sys
 
 if __name__ == "__main__":
 
@@ -6,9 +7,22 @@ if __name__ == "__main__":
 
         lines = f.readlines()
 
+        buffer_length = 0
+        string_length = 0
         for i, line in enumerate(lines):
 
             ln = line.strip('\n')
 
             if not ln:
                 continue
+
+            line_list = list(ln)
+
+            buffer_length += len(line_list)
+
+            string = ln.strip("\"")
+            doublequote_count = string.count('"')
+            backslash_count = string.count('\\\\')
+            x_count = string.count('\\x')
+            print "%s, %s -- %d - %d, (%d %d %d)" % (ln, string, len(line_list), len(string), doublequote_count, backslash_count, x_count)
+
