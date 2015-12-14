d2a061676761f74211edfe5dff0301648d7d2ee1
Stavros Vagionitis <stavros.vagionitis@gmail.com>
2015-12-14 00:16:00 +0000

    Day 12: Add part two in README and an attempt to solve the second part

--- adventofcode.com/12/sol12.py
+++ adventofcode.com/12/sol12.py
@@ -1,4 +1,26 @@
 import re
+import json
+
+def get_obj(obj, value=None, lst=[]):
+    """ Returns a list with all the objects with value
+    """
+
+    if type(obj) is dict:
+        if value not in obj.values():
+            for v in obj.values():
+                if type(v) is dict or type(v) is list:
+                    get_obj(v, value = value, lst = lst)
+        else:
+            print '-->', json.dumps(obj)
+            lst.append(obj)
+    elif type(obj) is list:
+        for v in obj:
+            if type(v) is dict:
+                get_obj(v, value = value, lst = lst)
+
+
+    return lst
+
 
 if __name__ == "__main__":
 
@@ -14,21 +36,26 @@ if __name__ == "__main__":
                 continue
 
 
-            # Remove the {,},[,]
-            ln = ln.replace('{','').replace('}','').replace('[', '').replace(']','')
-
-            # Remove the strings
-            ln = re.sub('"\w+"', '', ln)
-
-            # Split by , or :
-            l = re.split(',|:', ln)
-
-            # Convert to set to squash dublicates
-            l = set(l)
-            if '' in l:
-                l.remove('')
+            # Find all numbers in the format -12.345
+            l = re.findall("-?\d+(?:\.\d+)?", ln)
 
             # Convert to int
             l = [int(i) for i in l]
 
-            print sum(l)
+            print '1. What is the sum of all numbers in the document? %d' % sum(l)
+
+            # Load json in python data structures
+            j = json.loads(ln)
+
+            # Find all the objects with value red
+            ln_red = get_obj(j, value='red', lst=[])
+
+            #print ln_red, len(ln_red)
+            ln_red = json.dumps(ln_red)
+
+            l_red = re.findall("-?\d+(?:\.\d+)?", ln_red)
+
+            l_red = [int(i) for i in l_red]
+
+            print sum(l_red), sum(l) - sum(l_red)
+
