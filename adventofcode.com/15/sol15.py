532c78d35494e0cec62ac6271a2361d20a7e2071
Stavros Vagionitis <svagionitis@tycoint.com>
2015-12-15 15:55:55 +0000

    Day 15: Initial commit for day 15

---
+++ adventofcode.com/15/sol15.py
@@ -0,0 +1,13 @@
+if __name__ == "__main__":
+
+    with open("input.txt") as f:
+
+        lines = f.readlines()
+
+        for i, line in enumerate(lines):
+
+            ln = line.strip('\n')
+
+            if not ln:
+                continue
+
